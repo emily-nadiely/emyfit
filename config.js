bf1245ad4514be3d6330afa84ccb47298df8dab0
EmyNadiely <emy.nadiely@gmail.com>
2026-07-21 17:28:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 window.EMYFIT_CONFIG={
-  supabaseUrl:"https://hvrxqhgjlaiawcetgpwl.supabase.co
-",
+  supabaseUrl:"https://hvrxqhgjlaiawcetgpwl.supabase.co",
   supabaseAnonKey:"sb_publishable_mElz4zX9thasvw_hEEA4Cw_OZ6yR0vO",
   aiFunctionUrl:"",
   appVersion:"2.0.0"
